ec8119d3372ffddd8e3b8cc7228e6a43fe34a2bc
mdyer2 <132947537+mdyer2@users.noreply.github.com>
2024-07-29 14:39:25 -0400

    Update application.py

--- application.py
+++ application.py
@@ -1,23 +1,25 @@
 from flask import Flask
-from werkzeug.middleware.proxy_fix import ProxyFix
-from extensions import db
 from configuration import Config
+from extensions import db
 from routes import register_routes
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    
+    db.init_app(app)
+    
+    with app.app_context():
+        db.create_all()
 
-# Use ProxyFix to handle proxy headers
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
-
-# Initialize the database
-db.init_app(app)
-
-# Register routes
-register_routes(app)
+    register_routes(app)
+    
+    return app
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app = create_app()
+    app.run(host='0.0.0.0', port=8000)
+
 
 
 
